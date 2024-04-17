0a62f0fa7eadc3b9066a7e5d5c3e7154339a48ac
programmer Kartier <progrmmerKartier@gmail.com>
2024-04-17 11:24:28 +0200

    ts

--- AA/src/app/landingpage/landingpage.component.ts
+++ AA/src/app/landingpage/landingpage.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-landingpage',
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
-export class LandingpageComponent {
+export class LandingpageComponent implements OnInit {
+
+  ngOnInit() {
+    function toggleMenu() {
+      const menu = document.querySelector('.burger-menu') as HTMLElement;
+      menu.classList.toggle('open');
+    }    
+  }
 
 }
