7002c70556374a2b35b615ac3cd8bd4a075e589d
programmer Kartier <progrmmerKartier@gmail.com>
2024-04-18 17:38:19 +0200

    ts

--- AA/src/app/landingpage/landingpage.component.ts
+++ AA/src/app/landingpage/landingpage.component.ts
@@ -10,10 +10,45 @@ export class LandingpageComponent implements OnInit {
   ngOnInit() {
 
       const menu = document.querySelector('.burger-menu') as HTMLElement;
+      const header = document.querySelector("#header") as HTMLElement;
+      const floatingNav = document.querySelector("#floating-menu") as HTMLElement;
+      const faqChildDiv = document.querySelectorAll(".child-div") as NodeListOf<HTMLElement>;
+
+      var headerHeight = header.offsetHeight;
+      floatingNav.style.top = headerHeight + "px";
+
+      var menuFlag = false;
 
       menu.addEventListener("click", () => {
-        menu.classList.toggle('open');
-      })
+        menu.classList.toggle('open'); 
+        if (!menuFlag) {
+          floatingNav.style.display = "flex";
+          menuFlag = true;
+        } else {
+          floatingNav.style.display = "none";
+          menuFlag = false;
+        }
+      });
+
+      const answers = document.querySelectorAll(".faq-ans") as NodeListOf<HTMLElement>;
+
+      faqChildDiv.forEach(child => {
+
+        child.addEventListener("click", () => {
+
+          answers.forEach(answer => {
+            answer.style.display = "flex";
+
+            if (answer.style.display === "flex") {
+              child.addEventListener("click", ()=> {
+                answer.style.display = "none";
+              })
+            }
+          });
+
+        });
+
+      });
       
   }
 
