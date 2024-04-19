23d9fc00080b84bba19289e35a3e7939dc2cf336
programmer Kartier <progrmmerKartier@gmail.com>
2024-04-19 05:42:15 +0200

    ts

--- AA/src/app/security/register/register.component.ts
+++ AA/src/app/security/register/register.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    const subBtn = document.querySelector(".sub-btn") as HTMLElement;
+    const formDiv = document.querySelector(".field-input-parent") as HTMLElement;
+
+    var formDivLength = formDiv.offsetWidth;
+    subBtn.style.width = formDivLength + "px"
+
+    
+  }
 
 }
--- AA/src/app/security/security.component.ts
+++ AA/src/app/security/security.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-security',
   templateUrl: './security.component.html',
   styleUrl: './security.component.css'
 })
-export class SecurityComponent {
+export class SecurityComponent implements OnInit {
+
+  ngOnInit(): void {
+      const regBtn = document.querySelector("#register-btn") as HTMLElement;
+      const logBtn = document.querySelector("#login-btn") as HTMLElement;
+
+      regBtn.addEventListener("click", () => {
+        window.location.href = "security/register"
+      });
+
+      logBtn.addEventListener("click", () => {
+        window.location.href = "security/login"
+      })
+  }
 
 }
