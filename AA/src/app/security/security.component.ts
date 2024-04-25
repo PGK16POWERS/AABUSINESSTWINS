988529c2a6e99d130545a49d02b6366606835e77
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-04-25 09:54:28 +0200

    touch

--- AA/src/app/security/security.component.ts
+++ AA/src/app/security/security.component.ts
@@ -8,16 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class SecurityComponent implements OnInit {
 
   ngOnInit(): void {
-      const regBtn = document.querySelector("#register-btn") as HTMLElement;
-      const logBtn = document.querySelector("#login-btn") as HTMLElement;
+      const regBtn = document.querySelector("#mail-opt") as HTMLElement;
 
       regBtn.addEventListener("click", () => {
         window.location.href = "security/register"
       });
 
-      logBtn.addEventListener("click", () => {
-        window.location.href = "security/login"
-      })
   }
 
 }
