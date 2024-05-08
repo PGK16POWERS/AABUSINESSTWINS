c8e74ecc232a4181f0e7c589d761774596f0325e
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-05-08 19:45:20 +0200

    ts

--- AA/src/app/landingpage/landingpage.component.ts
+++ AA/src/app/landingpage/landingpage.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class LandingpageComponent implements OnInit {
 
   checkData = {
+    fullName:'',
+    phoneNumber: '',
     email: ''
   };
 
