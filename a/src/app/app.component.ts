3637577b932c2e835bb8f66c828cb5e2f6659ad4
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-05-31 16:02:12 +0200

    ts

--- a/src/app/app.component.ts
+++ a/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'a';
+
+  ngOnInit() {
+    const serviceChildren = document.querySelectorAll(".service-child") as NodeListOf<HTMLElement>;
+
+    serviceChildren.forEach(child => {
+
+      const viewDetailLink = child.querySelector(".service-link") as HTMLElement;
+      const detailParent = child.querySelector(".details-div") as HTMLElement;
+      const hideLink = child.querySelector(".hide-link") as HTMLElement;
+
+      viewDetailLink.addEventListener("click", () => {
+        detailParent.style.display = "flex";
+        child.style.width = "100%";
+        viewDetailLink.style.display = "none";
+      });
+
+      hideLink.addEventListener("click", () => {
+        detailParent.style.display = "none";
+        child.style.width = "";
+        viewDetailLink.style.display = "flex";
+      });
+
+    });
+  }
 }
