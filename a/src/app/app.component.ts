52a19e047c1f6526c8e04c7c736a7fb123e8ddf2
PGK16POWERS <136324051+PGK16POWERS@users.noreply.github.com>
2024-06-05 21:32:47 +0200

    styls

--- a/src/app/app.component.ts
+++ a/src/app/app.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit {
-  title = 'a';
+export class AppComponent implements OnInit, OnDestroy {
+  title = 'AA Business Agency';
+
+  images: string[] = [
+    'assets/aa_twins.png',
+    'assets/newtouches/amogelang.svg',
+    'assets/newtouches/atlegang.svg'
+  ];
+
+  currentImageIndex: number = 0;
+  intervalId: any;
 
   ngOnInit() {
+    this.startImageSlideshow();
     const serviceChildren = document.querySelectorAll(".service-child") as NodeListOf<HTMLElement>;
     const heroHeader = document.querySelector(".hero-header-small") as HTMLElement;
+    const contactButton = document.querySelectorAll(".contact-btn") as NodeListOf<HTMLElement>;
+    const callDiv = document.querySelector("#call-div") as HTMLElement;
+    const emailDiv = document.querySelector("#email-div") as HTMLElement;
     let sentenceIndex = 0;
     let charIndex = 0;
 
@@ -28,12 +41,15 @@ export class AppComponent implements OnInit {
           child.style.display = "flex";
         });
 
+        child.style.backgroundImage = "url(assets/newtouches/serv-bg.jpg)";
+        child.style.backgroundSize = "cover";
+
         child.style.width = "100%";
         child.style.height = "100%";
 
         detailParent.style.display = "flex";
 
-        viewDetailLink.style.display = "none"
+        viewDetailLink.style.display = "none";
       });
 
       hideLink.addEventListener("click", () => {
@@ -42,6 +58,9 @@ export class AppComponent implements OnInit {
         serviceChildren.forEach(kid => {
           kid.style.display = "flex";
         });
+
+        child.style.backgroundImage = "none";
+        child.style.backgroundSize = "none";
 
         child.style.width = "";
         child.style.height = "";
@@ -79,5 +98,29 @@ export class AppComponent implements OnInit {
   heroHeader.innerHTML = "";
   typeWriter();
 
+  contactButton.forEach(child => {
+    child.addEventListener("click", () => {
+      window.location.href = "#contact-section";
+    })
+  });
+
+  callDiv.addEventListener("click", () => {
+    window.location.href = "tel:+270714882177"
+  });
+
+  emailDiv.addEventListener("click", () => {
+    window.location.href = "mailto:amogelangmangoale215@gmail.com"
+  })
+
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.intervalId);
+  }
+
+  startImageSlideshow() {
+    this.intervalId = setInterval(() => {
+      this.currentImageIndex = (this.currentImageIndex + 1) % this.images.length;
+    }, 4000); // Change image every 4 seconds
   }
 }
